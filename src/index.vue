b6f31aa5394d6535d6e5b5c43a41da03ed0deeb9
Вадим Гараев <garaev.vm@gazprom-neft.ru>
2025-04-14 14:16:04 +0300

    Fixes index.vue

--- src/index.vue
+++ src/index.vue
@@ -2,60 +2,34 @@
 import { ref } from 'vue';
 import { PrizmButton, PrizmTag, PrizmInput, PrizmDatePicker } from './components';
 
-const mockComponentsData = [
-  {
-    id: 1,
-    name: 'Loren data',
-    type: 'default',
-    outline: false,
-  },
-  {
-    id: 2,
-    name: 'Loren data',
-    type: 'danger',
-    outline: true,
-  },
-  {
-    id: 3,
-    name: 'Loren data',
-    type: 'warning',
-    outline: false,
-  },
-  {
-    id: 4,
-    name: 'Loren data',
-    type: 'primary',
-    outline: true,
-  },
-  {
-    id: 5,
-    name: 'Loren data',
-    type: 'success',
-    outline: false,
-  },
-];
-
 const datePicker = ref('');
 </script>
 <template>
   <div :class="classes.root">
     <div :class="classes.group">
-      <PrizmButton
-        v-for="mockComponentData in mockComponentsData"
-        :key="mockComponentData.id"
-        :type="mockComponentData.type"
-        :plain="mockComponentData.outline"
-        >{{ mockComponentData.name }}</PrizmButton
-      >
+      <PrizmButton>Привет</PrizmButton>
+
+      <PrizmButton type="primary">Привет</PrizmButton>
+
+      <PrizmButton type="info">Привет</PrizmButton>
+
+      <PrizmButton type="success">Привет</PrizmButton>
+
+      <PrizmButton type="danger">Привет</PrizmButton>
+
+      <PrizmButton type="warning">Привет</PrizmButton>
     </div>
 
     <div :class="classes.group">
-      <PrizmTag
-        v-for="mockComponentData in mockComponentsData"
-        :key="mockComponentData.id"
-        :type="mockComponentData.type"
-        >{{ mockComponentData.name }}</PrizmTag
-      >
+      <PrizmTag>Привет</PrizmTag>
+
+      <PrizmTag type="danger">Привет</PrizmTag>
+
+      <PrizmTag type="primary">Привет</PrizmTag>
+
+      <PrizmTag type="warning">Привет</PrizmTag>
+
+      <PrizmTag type="success">Привет</PrizmTag>
     </div>
 
     <div :class="classes.group">
