f9fd240b20eda942ae03c858e0201a15ba180cdf
Вадим Гараев <garaev.vm@gazprom-neft.ru>
2025-04-29 13:47:25 +0300

    Fixes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import './style.scss';
 export * from './components';
 export * from './shared/icon';
-export type { FormInstance, FormRules } from 'element-plus';
 
 import { createApp } from 'vue';
 import index from './index.vue';
