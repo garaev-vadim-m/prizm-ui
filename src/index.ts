d40a57074f834b765182675ede7c29ba6ebd19d4
Вадим Гараев <garaev.vm@gazprom-neft.ru>
2025-04-29 14:27:24 +0300

    Fixes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import './style.scss';
 export * from './components';
 export * from './shared/icon';
 
-// import { createApp } from 'vue';
-// import index from './index.vue';
-// const app = createApp(index);
-// app.mount('#app');
+import { createApp } from 'vue';
+import index from './index.vue';
+const app = createApp(index);
+app.mount('#app');
